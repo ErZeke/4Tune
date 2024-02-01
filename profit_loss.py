acae4d22cd76abd9b40f86253486e9500436ce10
paigejoseph <cpaigejoseph@gmail.com>
2024-02-01 14:28:58 +0800

    Added more comments

--- profit_loss.py
+++ profit_loss.py
@@ -34,8 +34,9 @@ def netprofitdifferences():
         if increase==len(netprofit_diff): #checking if net profit is constantly increasing in profits
             index=netprofit_diff.index(max(netprofit_diff)) #max(netprofit_diff) looks for the maximum value, netprofit_diff.index(__) gets the index of the maximum value in the list
             file.write("\n[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
-            file.write(f"[HIGHEST NET PROFIT SURPLUS] DAY: {profitloss[index][0]+1}, AMOUNT: SGD{max(netprofit_diff)}\n") #we used i+1 to help to iterate through the csv file to retrieve the value. 
-
+            file.write(f"[HIGHEST NET PROFIT SURPLUS] DAY: {profitloss[index][0]+1}, AMOUNT: SGD{max(netprofit_diff)}\n") 
+            #we used i+1 to help to iterate through the csv file to retrieve the value. Instead of using [i][0] which will only access the day column 
+            #and we have already calculated the net profit difference in the earlier parts of the code so using the same iterator makes it easier.
         elif decrease==len(netprofit_diff): #is constantly decreasing in profits
             index=netprofit_diff.index(min(netprofit_diff)) #min(netprofit_diff) looks for the minimum value, netprofit_diff.index(__) gets the index of the minimum value in the list
             file.write("\n[NET PROFIT DEFICIT] NET PROFIT ON EACH DAY IS LOWER THAN THE PREVIOUS DAY\n")
@@ -46,15 +47,15 @@ def netprofitdifferences():
             file.write("\nDays with Fluctuations in Net Profits:\n")
             deficit={} #using a dictionary to store all deficit days and values
             for i in range(len(netprofit_diff)): #iterating through all differences
-                if netprofit_diff[i]<0: #is deficit as its a negative number
+                if netprofit_diff[i]<0: #to identify which is a deficit (deficit as its a negative number)
                     deficit[profitloss[i][0]+1]=netprofit_diff[i] #appending day and difference to the dictionary
-                    file.write(f"[NET PROFIT DEFICIT] DAY: {profitloss[i][0]+1}, AMOUNT: SGD{-netprofit_diff[i]}\n")
+                    file.write(f"[NET PROFIT DEFICIT] DAY: {profitloss[i][0]+1}, AMOUNT: SGD{-netprofit_diff[i]}\n") #to write the net profit deficit in the proper format in the summary text file. 
 
             temp_list=sorted(netprofit_diff.copy()) #this sorts the difference list from smallest to largest values
             temp_list=temp_list[0:3] #obtaining the first 3 values of the sorted list the top 3 most negative values
 
             file.write("\nTop 3 Highest Deficit Amounts:\n")
             for i in range(3): #iterating through the 3 most negative values
-                position=['','2ND ','3RD '] #to do the rankings for cash deficit values
+                position=['','2ND ','3RD '] #to do the rankings for net profit deficit values
                 index=netprofit_diff.index(temp_list[i]) #temp_list[i] is the difference value, netprofit_diff.index(__) looks for its original position to correlate back to it's day
-                file.write(f"[{position[i]}HIGHEST NET PROFIT DEFICIT] DAY: {profitloss[index][0]+1}, AMOUNT: SGD{-temp_list[i]}\n")
+                file.write(f"[{position[i]}HIGHEST NET PROFIT DEFICIT] DAY: {profitloss[index][0]+1}, AMOUNT: SGD{-temp_list[i]}\n") #to write the net profit deficit in the proper format in the summary text file. 
